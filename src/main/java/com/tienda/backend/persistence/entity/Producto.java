f8b77fe95b7faf1900268b728648b697890f50cd
Alexis <omaroquendo221@gmail.com>
2021-01-21 18:17:14 -0500

    Mapeo de la tabla con la clase

---
+++ src/main/java/com/tienda/backend/persistence/entity/Producto.java
@@ -0,0 +1,58 @@
+package com.tienda.backend.persistence.entity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "productos")
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idproducto")
+    private Integer idProducto;
+
+    private String image;
+    private  String title;
+    private BigDecimal price;
+    private String description;
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
