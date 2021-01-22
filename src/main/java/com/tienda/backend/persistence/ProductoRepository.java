039c62b61d05df59704228b19ad76cf906d2a534
Alexis <omaroquendo221@gmail.com>
2021-01-22 18:44:30 -0500

    Uso de la anotacion Repository

--- src/main/java/com/tienda/backend/persistence/ProductoRepository.java
+++ src/main/java/com/tienda/backend/persistence/ProductoRepository.java
@@ -2,13 +2,21 @@ package com.tienda.backend.persistence;
 
 import com.tienda.backend.persistence.crud.ProductoCrudRepository;
 import com.tienda.backend.persistence.entity.Producto;
-
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
--- src/main/java/com/tienda/backend/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/tienda/backend/persistence/crud/ProductoCrudRepository.java
@@ -3,5 +3,8 @@ package com.tienda.backend.persistence.crud;
 import com.tienda.backend.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+       
 }
